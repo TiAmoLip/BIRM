a7f223a57c0f62892f11ffa244aa43c5db54419a
TiAmoLip <1239261229@qq.com>
2023-12-25 23:34:20 +0800

    fix bug

--- train.py
+++ train.py
@@ -18,11 +18,7 @@ from utils import CMNIST_LYDP,make_mnist_envs,concat_envs
 
 parser = argparse.ArgumentParser(description='Colored MNIST')
 parser.add_argument('--envs_num', type=int, default=2)
-parser.add_argument('--batch_size', type=int, default=1024)
 parser.add_argument('--seed', type=int, default=0)
-parser.add_argument('--prior_sd_coef', type=int, default=1200)
-parser.add_argument('--l2_regularizer_weight', type=float,default=0.001)
-parser.add_argument('--print_every', type=int,default=20)
 parser.add_argument('--shape', type=int,default=28,help="shape of colored mnist",choices=[28,14])
 parser.add_argument('--data_num', type=int, default=2000)
 parser.add_argument('--lr', type=float, default=0.001)
@@ -30,14 +26,13 @@ parser.add_argument('--env_type', default="linear", type=str, choices=["2_group"
 parser.add_argument('--irm_type', default="birm", type=str, choices=["birm", "irmv1", "erm"])
 
 parser.add_argument('--hidden_dim', type=int, default=16)
-parser.add_argument('--step_gamma', type=float, default=0.1)
+
 parser.add_argument('--penalty_anneal_iters', type=int, default=200)
-parser.add_argument('--penalty_weight', type=float, default=10000.0)
+parser.add_argument('--_lambda_', type=float, default=10000.0)
 parser.add_argument('--steps', type=int, default=501)
-parser.add_argument('--grayscale_model', type=int, default=0)
+
 parser.add_argument('--sampleN', type=int, default=10)
 parser.add_argument('--wandb_log_freq',type=int,default=-1)
-parser.add_argument('--model',type=str,default='MLP',choices=['MLP','CNN'])
 parser.add_argument('--device',type=int,default=-1)
 
 
@@ -76,18 +71,19 @@ with tqdm(total=flags.steps) as pbar:
         q_u.fit(combined_envs[0],combined_envs[1],f_e,20)
         loss = 0
         opt = torch.optim.Adam(f_e.parameters(),lr=1e-3)
-        for _ in range(10):
+        for _ in range(flags.sampleN):
             epsilon = torch.randn_like(q_u.m_u)
             [ loss := loss + (1+_lambda_)*q_u.recon_loss(env_tr['images'],env_tr['labels'], q_u.sample(epsilon), f_e)-_lambda_*q.recon_loss(env_tr['images'],env_tr['labels'], q.sample(epsilon), f_e)  for q, env_tr in zip(q_u_e, train_envs) ]
         opt.zero_grad()
+        loss = loss/flags.sampleN
         loss.backward()
         opt.step()
         
         
         epsilon = torch.zeros_like(q_u.m_u)
         classifier = q_u.sample(epsilon)
-        if step>0:
-            _lambda_=0.1
+        if step>flags.penalty_anneal_iters:
+            _lambda_=flags._lambda_
             f_e.eval()
             classifier.eval()
             with torch.no_grad():
